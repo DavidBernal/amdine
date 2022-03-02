e0e01d12097def660649c9066b2a117241c44ca6
David Bernal <dbl1987@gmail.com>
2022-03-02 15:22:49 +0100

    Improve define function

--- index.ts
+++ index.ts
@@ -1,47 +1,135 @@
 import { globby } from 'globby';
 import { join } from 'path';
 
-export default (function () {
-  // TODO: Improve algorithm to verify first if all dependencies are satisfied
-  const _deps = {};
-  const stack = [];
-  const libs = Promise.resolve();
+const amdine = (function () {
+  //#region types
+  type ModuleWithDependenciesDefinition = [name: string, dependencies: string[], factory: Function];
+  type Module = string | number | boolean | object | symbol;
+  type InitOptions = { glob?: string };
 
-  type InitOptions = {
-    glob?: string;
-  };
+  /** Possible Params signature */
+  type UnnamedModule = [factory: Function];
+  type NamedModule = [name: string, factoryOrValue: Function | Module];
+  type UnnamedModuleWithDependencies = [dependencies: string[], factory: Function];
+  type NamedModuleWithDependencies = [name: string, dependencies: string[], factory: Function];
 
-  async function init(options: InitOptions = {}) {
+  type Params =
+    | NamedModule
+    | UnnamedModule
+    | NamedModuleWithDependencies
+    | UnnamedModuleWithDependencies;
+
+  //#endregion
+
+  //#region type guards
+  function isUnnamedModule(params: Params): params is UnnamedModule {
+    return params.length === 1 && typeof params[0] === 'function';
+  }
+
+  function isNamedModule(m: Params): m is NamedModule {
+    return typeof m[0] === 'string' && typeof m[1] === 'function';
+  }
+
+  function isUnnamedModuleWithDependencies(m: Params): m is UnnamedModule {
+    return Array.isArray(m[0]) && typeof m[1] === 'function';
+  }
+
+  function isNamedModuleWithDependencies(m: Params): m is NamedModuleWithDependencies {
+    return (m.length === 3 && typeof m[0] === 'string' && Array.isArray(m[1]) && typeof m[2] === 'function');
+  }
+  //#endregion
+
+  //#region internal variables
+  const dependencies: Record<string, Module> = {};
+
+  // first: external dependencies
+  const unnamedModules = [];
+
+  // second: internal dependencies
+  const namedModules = [];
+
+  // third: more complex dependencies
+  const namedModulesWithDependencies = [];
+
+  // fourth: entrypoints and similars
+  const unnamedModulesWithDependencies = [];
+
+  //#endregion
+
+  //#region private methods
+  async function defineUnnamedModule([factory]: UnnamedModule) {
+    unnamedModules.push(factory());
+  }
+
+  async function defineNamedModule([name, value]: NamedModule) {
+    namedModules.push(async () => {
+      if (value instanceof Function) {
+        dependencies[name] = await value();
+      } else {
+        dependencies[name] = value;
+      }
+    });
+  }
+
+  async function defineNamedModuleWithDependencies([name,deps,factory]: NamedModuleWithDependencies) {
+    namedModulesWithDependencies.push([name, deps, factory]);
+  }
+
+  function defineUnnamedModuleWithDependencies([deps,factory]: UnnamedModuleWithDependencies) {
+    unnamedModulesWithDependencies.push(factory(...deps.map((dep) => dependencies[dep])));
+  }
+
+  //#endregion
+
+  //#region public methods
+  async function define(...args: Params) {
+    if (isNamedModule(args)) {
+      defineNamedModule(args);
+    } else if (isUnnamedModule(args)) {
+      defineUnnamedModule(args);
+    } else if (isUnnamedModuleWithDependencies(args)) {
+      defineUnnamedModuleWithDependencies(args);
+    } else if (isNamedModuleWithDependencies(args)) {
+      defineNamedModuleWithDependencies(args);
+    }
+  }
+
+  async function discovery(options: InitOptions = {}) {
     const glob = options.glob || '*/**/*.(j|t)s';
     const paths = await globby([glob, '!node_modules/**/*', '!**/*.d.ts'], {
       cwd: process.cwd(),
       expandDirectories: { extensions: ['ts', 'js'] },
     });
-    console.log('paths', paths);
 
     for await (const p of paths) {
       console.log('importing ', './' + p);
       await import(join(process.cwd(), p));
     }
+  }
 
-    await libs;
+  // TODO: Improve algorithm to verify first if all dependencies are satisfied
+  async function init() {
+    await Promise.all(unnamedModules);
+    await Promise.all(namedModules);
 
-    let i = stack.length;
+    let i = namedModulesWithDependencies.length;
     let completeRound = i * i;
-    while (stack.length) {
+    while (namedModulesWithDependencies.length) {
       completeRound--;
-      const [name, deps, factory] = stack.shift();
-      if (deps.every((dep) => dep in _deps)) {
-        _deps[name] = await factory(...deps.map((dep) => _deps[dep]));
+      const [name, deps, factory] = namedModulesWithDependencies.shift();
+      if (deps.every((dep) => dep in dependencies)) {
+        dependencies[name] = await factory(
+          ...deps.map((dep) => dependencies[dep])
+        );
       } else {
-        stack.push([name, deps, factory]);
+        namedModulesWithDependencies.push([name, deps, factory]);
       }
 
       if (completeRound == 0) {
-        if (stack.length === i) {
-          const problematicDeps = stack
+        if (namedModulesWithDependencies.length === i) {
+          const problematicDeps = namedModulesWithDependencies
             .flatMap(([, d]) => d)
-            .filter((d) => !(d in _deps))
+            .filter((d) => !(d in dependencies))
             .filter((d, j, arr) => arr.indexOf(d) === j);
 
           console.error(
@@ -51,32 +139,29 @@ export default (function () {
 
           console.error(
             'Modules no resolved: ',
-            stack.map(([n]) => n).join(',')
+            namedModulesWithDependencies.map(([n]) => n).join(',')
           );
 
           process.exit(1);
         }
-        i = stack.length;
+        i = namedModulesWithDependencies.length;
         completeRound = i * i;
       }
     }
+
+    await Promise.all(unnamedModulesWithDependencies);
   }
 
-  async function define(name: string, value: any) {
-    if (value instanceof Function) {
-      _deps[name] = await value();
-    } else {
-      _deps[name] = value;
-    }
+  function discoveryAndInit(options: InitOptions = {}) {
+    discovery(options).then(() => init());
   }
+  //#endregion
 
-  function defineWithDependencies(
-    name: string,
-    deps: string[],
-    factory: Function
-  ) {
-    stack.push([name, deps, factory]);
-  }
-
-  return { define, defineWithDependencies, init };
+  return { define, init, discovery, discoveryAndInit };
 })();
+
+export default amdine;
+export const init = amdine.init;
+export const discovery = amdine.discovery;
+export const discoveryAndInit = amdine.discoveryAndInit;
+export const define = amdine.define;
